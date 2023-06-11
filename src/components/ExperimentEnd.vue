85fcd60182152f64c79758f53f3295a65052160c
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-06-11 16:10:09 +0900

    perf

--- src/components/ExperimentEnd.vue
+++ src/components/ExperimentEnd.vue
@@ -30,18 +30,32 @@ function handleNext() {
 </script>
 
 <template>
-  <div class="flex flex-col gap-8 px-8 text-2xl">
-    <h1 class="font-bold text-4xl">実験課題はこれで終わります</h1>
-    <p>あなたの６桁の確認番号は：{{ getExperimentIdentifier() }}</p>
-    <p>「コピー」ボタンを押して，クリップボードにコピーしてください</p>
-    <p>この後，課題中に思い出した刺激を入力してもらいます</p>
-    <p>下のボタンをを押して入力フォームを提出してください</p>
-    <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      @click="handleNext"
-    >
-      確認番号をコピーして入力フォームに移動する
-    </button>
+  <div class="flex columns-2">
+    <div class="flex flex-col gap-8 px-8 text-2xl">
+      <h1 class="font-bold text-4xl">実験課題はこれで終わります</h1>
+      <p>あなたの６桁の確認番号は：{{ getExperimentIdentifier() }}</p>
+      <p>「コピー」ボタンを押して，クリップボードにコピーしてください</p>
+      <p>この後，課題中に思い出した刺激を入力してもらいます</p>
+      <p>下のボタンをを押して入力フォームを提出してください</p>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded max-w-2xl"
+        @click="handleNext"
+      >
+        確認番号をコピーして入力フォームに移動する
+      </button>
+      <p>
+        ボタンで入力フォームに移動できない場合には，<br />
+        リンクをクリックし，またはQRコードをスキャンして<br />
+        フォームに入力してください：<br />
+        <a
+          class="underline text-yellow-500"
+          :href="settings.formUrl"
+          target="_blank"
+          >{{ settings.formUrl }}</a
+        >
+      </p>
+    </div>
+    <img class="max-w-screen-sm" :src="settings.qrCodeImgUrl" alt="QRコード" />
   </div>
 </template>
 
--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,7 @@
 export const settings = {
   waitTime: 0.1, // seconds
-  formUrl: 'https://forms.office.com/r/nYNHnrjRvq',
+  formUrl: 'https://forms.office.com/r/3UnE4zT08C',
+  qrCodeImgUrl: '/qrcode.png',
   waitTimeBeforeCalculate: 3, // seconds
   calculateDuration: 3, // seconds
   recallDuration: 3, // seconds
