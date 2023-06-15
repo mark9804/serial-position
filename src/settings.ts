dff487c12022723706b50d40c24790d7a2fb888a
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-06-15 13:13:06 +0900

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 export const settings = {
-  waitTime: 0.1, // seconds
-  wordInterval: 0.2, // seconds
+  waitTime: 1, // seconds
+  wordInterval: 2, // seconds
   formUrl: 'https://forms.office.com/r/3UnE4zT08C',
   qrCodeImgUrl: '/qrcode.png',
-  waitTimeBeforeCalculate: 2, // seconds
-  calculateDuration: 3, // seconds
-  recallDuration: 1, // seconds
+  waitTimeBeforeCalculate: 5, // seconds
+  calculateDuration: 30, // seconds
+  recallDuration: 60, // seconds
 };
