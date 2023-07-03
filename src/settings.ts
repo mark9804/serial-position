306ddb77f08269e2399580189af7eb9524968ac7
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-07-03 18:09:49 +0900

    routine

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 export const settings = {
   waitTime: 1, // seconds
   wordInterval: 2, // seconds
-  formUrl: 'https://forms.office.com/r/3UnE4zT08C',
+  formUrl: 'https://forms.office.com/r/hYrg4HBbdX',
   qrCodeImgUrl: '/qrcode.png',
   waitTimeBeforeCalculate: 5, // seconds
   calculateDuration: 60, // seconds
